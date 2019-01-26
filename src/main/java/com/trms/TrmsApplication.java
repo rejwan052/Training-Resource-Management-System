e35ce99884f88a222ecd7cafbaa0bc50cd36048d
Rejwan Ahmed <rejwan.052@gmail.com>
2019-01-27 00:35:48 +0600

    ## All in one

--- src/main/java/com/trms/TrmsApplication.java
+++ src/main/java/com/trms/TrmsApplication.java
@@ -3,7 +3,7 @@ package com.trms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 import javax.annotation.PostConstruct;
@@ -14,11 +14,11 @@ import java.util.TimeZone;
 		TrmsApplication.class,
 		Jsr310JpaConverters.class
 })
-public class TrmsApplication {
+public class TrmsApplication extends SpringBootServletInitializer {
 
 	@PostConstruct
 	void init() {
-		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT+6"));
 	}
 
 	public static void main(String[] args) {
